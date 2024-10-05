bf5e1912e0c495b784be287b5a486666b21a6530
jyothisable <athuljyothis18@gmail.com>
2024-10-06 04:16:42 +0530

    init

---
+++ loantap_credit_default_risk_model/predict.py
@@ -0,0 +1,12 @@
+import logging
+import os
+import sys
+
+# Get the path to the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) # root dir of project
+
+# Add the parent directory to the system path (can import from anywhere)
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+    
+
