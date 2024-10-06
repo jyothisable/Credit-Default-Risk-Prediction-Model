390b30bf6e0d0dd006738496665aa6599a3ca2ac
jyothisable <athuljyothis18@gmail.com>
2024-10-07 03:30:29 +0530

    Update data_handle methods for testing

--- tests/data_tests.py
+++ tests/data_tests.py
@@ -1,25 +1,24 @@
 import sys
 import os
-import pandas as pd
 
 # Get the path to the parent directory
-parent_dir = os.path.abspath(os.path.join(os.getcwd(), '..')) # root dir of project
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) # root dir of project
 
 # Add the parent directory to the system path (can import from anywhere)
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
     
-from loantap_credit_default_risk_model.data_handling import DataHandler
+from loantap_credit_default_risk_model.data_handling import load_data_and_sanitize
+from loantap_credit_default_risk_model import config
 
 # Test Data import
-data_import = DataHandler(file_path='data/raw/logistic_regression.csv')
-
-df = data_import.load_data()
-df = data_import.sanitize(df)
+df  = load_data_and_sanitize(config.FILE_NAME)
 
 def test_data_import():
-    assert df.shape == (396030, 27)
-
+    """
+    This test checks that the data has the expected number of columns.
+    """
+    assert df.shape[1] ==  27 , f"Expected 27 columns, got {df.shape[1]}"
 
 # Range Constrains    
 def test_data_range_loan_amnt():
