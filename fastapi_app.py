b331110c5b303c0b5a3fbb9625e37d5f096fb6ad
jyothisable <athuljyothis18@gmail.com>
2024-10-13 18:05:34 +0530

    Add FastAPI app

---
+++ fastapi_app.py
@@ -0,0 +1,59 @@
+# Importing Dependencies
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+import uvicorn
+import pandas as pd
+from loantap_credit_default_risk_model import data_handling
+
+app = FastAPI()
+
+class LoanFeatures(BaseModel):
+    loan_amnt: float
+    term: str
+    int_rate: float
+    installment: float
+    grade: str
+    sub_grade: str
+    emp_title: str
+    emp_length: str
+    home_ownership: str
+    annual_inc: float
+    verification_status: str
+    issue_d: str
+    purpose: str
+    title: str
+    dti: float
+    earliest_cr_line: str
+    open_acc: float
+    pub_rec: float
+    revol_bal: float
+    revol_util: float
+    total_acc: float
+    initial_list_status: str
+    application_type: str
+    mort_acc: float
+    pub_rec_bankruptcies: float
+    address: str
+
+model = data_handling.load_pipeline('XBG_model')
+
+@app.get('/')
+def index():
+    return {'message': 'Welcome to Loan Prediction App'}
+
+@app.post('/predict')
+async def predict(loan_data: LoanFeatures):
+    # Extract the data from the input
+    """
+    Predicts whether a loan should be approved or not based on the input data.
+    """
+    df = pd.DataFrame(jsonable_encoder(loan_data))
+    pred = model.predict(df)
+    if pred[0] == 0:
+        return {'Status of Loan Application': 'Approved'}
+    else:
+        return {'Status of Loan Application': 'Rejected'}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
